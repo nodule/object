be08d5addd101b51a867b0d1d46c6e00bfeeeb8b
Rob Halff <rob.halff@gmail.com>
2014-01-30 23:06:42 +0100

    beautify

--- nodes/isFinite/node.js
+++ nodes/isFinite/node.js
@@ -1,1 +1,5 @@
-if(underscore.isFinite(input.object)) { output.yes = true; } else { output.no = true; }
+if (underscore.isFinite(input.object)) {
+  output.yes = true;
+} else {
+  output.no = true;
+}
