3a4c979e4e61c6ff5de8ed1452b972dc7b3ea1c8
Rob Halff <rob.halff@gmail.com>
2014-01-31 21:24:11 +0100

    update fill

--- nodes/fill/node.js
+++ nodes/fill/node.js
@@ -1,13 +1,13 @@
 var obj = input.object ? input.object : {};
 
-if(input.keys.length !== input.value.length) {
+if(input.keys.length !== input.in.length) {
   output.error = {
     msg: 'Key length does not match value length'
   };
 } else {
 
   for(var i = 0; i < input.keys.length; i++) {
-   obj[input.keys[i]] = input.value[i];
+   obj[input.keys[i]] = input.in[i];
   }
 
   output.out = obj;
