fffa80b0088141f0ce2ab2ae5eaa8d248ea41243
Rob Halff <rob.halff@gmail.com>
2014-02-10 09:38:10 +0100

    update

--- nodes/group/node.js
+++ nodes/group/node.js
@@ -1,22 +1,27 @@
+function group(state, gid, chi) {
+
+  if (state[gid].complete &&
+    state[gid].total === (state[gid].items.length)) {
+
+    var g = chi.group('xout', output);
+    output({
+      out: state[gid].items
+    }, g.item());
+
+    g.done();
+
+    delete state[gid];
+
+  }
+
+}
+
 on.input. in = function () {
   // x contains our keys
   for (var gid in x) {
 
     state[gid].items.push(data);
-
-    if (state[gid].complete &&
-      state[gid].total === (state[gid].items.length)) {
-
-      var g = chi.group('xout', output);
-      output({
-        out: state[gid].items
-      }, g.item());
-
-      g.done();
-
-      delete state[gid];
-
-    }
+    group(state, gid, chi);
 
   }
 };
@@ -32,20 +37,6 @@ on.input.xin = function () {
     state[data.gid].total = data.items.length;
     state[data.gid].complete = true;
 
-    // ok sometimes at this point we already have everything...
-    // I wonder if the function stays in scope, i think not.
-    if (state[data.gid].complete &&
-      state[data.gid].total === (state[data.gid].items.length)) {
-
-      var g = chi.group('xout', output);
-      output({
-        out: state[data.gid].items
-      }, g.item());
-
-      g.done();
-
-      delete state[data.gid];
-
-    }
+    group(state, data.gid, chi);
   }
 };
