e168ccd1456e31c65b1b02d27f365be10ed3f2d4
Rob Halff <rob.halff@gmail.com>
2014-02-25 13:00:07 +0100

    add object move

---
+++ nodes/move/node.js
@@ -0,0 +1,13 @@
+// Receiving an array, check both are of the same length.
+if(input.from.length !== input.to.length) {
+  output.err = new Error('from length does not match to length');
+} else {
+
+  var doo = dot_object();
+  for(var i = 0; i < input.from.length; i++) {
+    doo.move(input.from[i], input.to[i], input.in);
+  }
+
+  output.out = input.in;
+
+}
