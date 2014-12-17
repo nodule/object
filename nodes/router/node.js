2cb38b0104705c9b354dbd80f442f2ff5c398645
Rob Halff <rob.halff@gmail.com>
2014-12-17 22:20:30 +0100

    update

--- nodes/router/node.js
+++ nodes/router/node.js
@@ -1,14 +1,27 @@
 on.input.in = function(data) {
 
   var out = [];
+  var reg;
+  var route;
   for(var i = 0; i < input.route.length; i++) {
-    var res = dot_object().pick(input.route[i], data);
+    route = input.route[i];
+    if(route.length !== 2) {
+      output({
+        error: new Error('Route should be in the form [path,match]')
+      });
+    }
+    var res = dot_object().pick(route[0], data);
     // err.. does pick remove the value?
     if(undefined !== res) {
-      out[i] = data; // send out the data, not the actual picked value.
-      output({out: out});
-      break;
+      reg = new RegExp(route[1]);
+      if(reg.test(res)) {
+        out[i] = data; // send out the data, not the actual picked value.
+        output({out: out});
+        return true;
+      }
     }
   }
-  // maybe send to error port if no matches.
+
+  output({missed: data});
+
 };
