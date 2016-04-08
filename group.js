549402f447177c45167e61b3f9daef434df2ac8b
Rob Halff <rob.halff@gmail.com>
2016-04-09 01:35:38 +0200

    use emitter

--- group.js
+++ group.js
@@ -13,36 +13,8 @@ module.exports = {
         type: "any",
         fn: function __XIN__(data, source, state, input, $, output, chix_group) {
           var r = function() {
-            state.group.confirm($.get('xin'))
-            state.handler($)
-              /*
-              if(!state.hasOwnProperty($.xin.gid)) {
-                state[$.xin.gid] = {
-                  items: [],
-                  total: null,
-                  complete: false
-                };
-              }
-
-              if ($.xin.complete) {
-                state[$.xin.gid].total = $.xin.items.length;
-                state[$.xin.gid].complete = true;
-
-                if (state[$.xin.gid].complete &&
-                  state[$.xin.gid].total === (state[$.xin.gid].items.length)) {
-
-                  var g = chi.group('xout', output);
-                  output({
-                    out: $.create(state[$.xin.gid].items)
-                  }, g.item());
-
-                  g.done();
-
-                  delete state[$.xin.gid];
-
-                }
-              }
-              */
+            state.group.receive($.get('xin'))
+            state.$ = $
           }.call(this);
           return {
             state: state,
@@ -56,36 +28,7 @@ module.exports = {
         fn: function __IN__(data, source, state, input, $, output, chix_group) {
           var r = function() {
             state.group.add($.get('in'))
-            state.handler($)
-
-            /*
-            // x contains our keys
-            for (var gid in x) {
-              if(!state.hasOwnProperty(gid)) {
-                state[gid] = {
-                  items: [],
-                  total: null,
-                  complete: false
-                };
-              }
-
-              state[gid].items.push($.get('in'));
-
-              if (state[gid].complete &&
-                state[gid].total === (state[gid].items.length)) {
-
-                var g = chi.group('xout', output);
-                output({
-                  out: $.create(state[gid].items)
-                }, g.item());
-
-                g.done();
-
-                delete state[gid];
-
-              }
-            }
-            */
+            state.$ = $
           }.call(this);
           return {
             state: state,
@@ -116,14 +59,12 @@ module.exports = {
     start: function __ONSTART__(data, source, state, input, $, output, chix_group) {
       var r = function() {
         state.group = chix_group.recv.create()
-        state.handler = function stateHandler($) {
-          if (state.group.isComplete()) {
-            output({
-              out: $.create(state.group.read())
-            })
-            state.group = chix_group.recv.create()
-          }
-        }
+        state.$ = null
+        state.group.on('group', function groupHandler(group) {
+          output({
+            out: state.$.create(group)
+          })
+        })
       }.call(this);
       return {
         state: state,
--- nodes/group/node.js
+++ nodes/group/node.js
@@ -1,77 +1,17 @@
 on.start = function start() {
   state.group = chix_group.recv.create()
-  state.handler = function stateHandler($) {
-    if (state.group.isComplete()) {
-      output({out: $.create(state.group.read())})
-      state.group = chix_group.recv.create()
-    }
-  }
+  state.$ = null
+  state.group.on('group', function groupHandler(group) {
+    output({out: state.$.create(group)})
+  })
 }
 
-// should be able to continously receive groups.
 on.input.in = function () {
   state.group.add($.get('in'))
-  state.handler($)
-
-  /*
-  // x contains our keys
-  for (var gid in x) {
-    if(!state.hasOwnProperty(gid)) {
-      state[gid] = {
-        items: [],
-        total: null,
-        complete: false
-      };
-    }
-
-    state[gid].items.push($.get('in'));
-
-    if (state[gid].complete &&
-      state[gid].total === (state[gid].items.length)) {
-
-      var g = chi.group('xout', output);
-      output({
-        out: $.create(state[gid].items)
-      }, g.item());
-
-      g.done();
-
-      delete state[gid];
-
-    }
-  }
-  */
+  state.$ = $
 };
 
 on.input.xin = function () {
-  state.group.confirm($.get('xin'))
-  state.handler($)
-  /*
-  if(!state.hasOwnProperty($.xin.gid)) {
-    state[$.xin.gid] = {
-      items: [],
-      total: null,
-      complete: false
-    };
-  }
-
-  if ($.xin.complete) {
-    state[$.xin.gid].total = $.xin.items.length;
-    state[$.xin.gid].complete = true;
-
-    if (state[$.xin.gid].complete &&
-      state[$.xin.gid].total === (state[$.xin.gid].items.length)) {
-
-      var g = chi.group('xout', output);
-      output({
-        out: $.create(state[$.xin.gid].items)
-      }, g.item());
-
-      g.done();
-
-      delete state[$.xin.gid];
-
-    }
-  }
-  */
+  state.group.receive($.get('xin'))
+  state.$ = $
 };
