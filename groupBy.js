dab335dc7b60fc080a137541431fb660c59083aa
Rob Halff <rob.halff@gmail.com>
2016-04-20 22:52:11 +0200

    new groupBy version

--- groupBy.js
+++ groupBy.js
@@ -6,69 +6,14 @@ module.exports = {
   phrases: {
     active: "Grouping object"
   },
-  expose: ["chi"],
   ports: {
     input: {
       "in": {
         title: "Object",
         type: "any",
-        fn: function __IN__(data, source, state, input, $, output) {
+        fn: function __IN__(data, source, state, input, $, output, chix_group) {
           var r = function() {
-            // x contains our keys
-            if (!x.hasOwnProperty(state.gid)) {
-              console.log(x)
-              throw Error('groupBy:in State gid not initialized yet', state.gid)
-            }
-            var id = x[state.gid];
-            if (!state[id]) {
-              state[id] = {};
-            }
-
-            state[id].in = $.get('in');
-
-            if (state[id].by) {
-              // we have a match.
-              if (!state.group[state[id].by]) {
-                state.group[state[id].by] = [];
-              }
-              state.group[state[id].by].push(state[id].in);
-            }
-
-            state.total++;
-
-            if (state.complete && state.length === (state.total / 2)) {
-              // send them out, might also create groups again.
-              for (var key in state.group) {
-                var g = chi.group('xout', output);
-
-                // like to introduce the grouped output, output like the below is grouped
-                // it's a bit like an array port, only difference the output goes to normal ports.
-                // I would like to add the convention, that if you send output this way.
-                // You must declare a group inside the json
-                //
-                // out: "group": "result"
-                // by:  "group": "result"
-                //
-                // in a ui this will look like:
-                // [o o] o o o
-                //
-                // Only two states:
-                //
-                //  [• •] and [o o]
-                //
-                // Whereas the individual ports are not perse synchronous:
-                //
-                //  [o o] o • o
-                //
-                // Where the first is the grouped output, you are assured those ports
-                // are pairs of data comming out, belong to eachother.
-                output({
-                  out: $.create(state.group[key]),
-                  by: $.create(JSON.parse(key)) // same as $.by
-                }, g.item());
-                g.done();
-              }
-            }
+            state.groupBy.add($.in)
           }.call(this);
           return {
             state: state,
@@ -79,22 +24,9 @@ module.exports = {
       xin: {
         title: "In Group",
         type: "any",
-        fn: function __XIN__(data, source, state, input, $, output) {
+        fn: function __XIN__(data, source, state, input, $, output, chix_group) {
           var r = function() {
-            // will always arrive first.
-            // probably this can all be done outside a component.
-            // the pair matching is something common.
-            if (!$.xin.complete) {
-              // register the groupId
-              // we rely on this being set first
-              state.gid = $.xin.gid;
-              state.complete = false;
-            } else {
-              // is finished
-              // send it out.
-              state.complete = true;
-              state.length = $.xin.items.length;
-            }
+            state.groupBy.receive($.xin)
           }.call(this);
           return {
             state: state,
@@ -105,37 +37,9 @@ module.exports = {
       by: {
         title: "Group By",
         type: "any",
-        fn: function __BY__(data, source, state, input, $, output) {
+        fn: function __BY__(data, source, state, input, $, output, chix_group) {
           var r = function() {
-            if (!x.hasOwnProperty(state.gid)) {
-              console.log(x)
-              throw Error('groupBy:by State gid not initialized yet', state.gid)
-            }
-
-            var id = x[state.gid];
-            if (!state[id]) state[id] = {};
-
-            state[id].by = JSON.stringify($.by); // keyify
-
-            if (state[id].in) {
-              // we have a match.
-              if (!state.group[state[id].by]) state.group[state[id].by] = [];
-              state.group[state[id].by].push(state[id].in);
-            }
-
-            state.total++;
-
-            if (state.complete && state.length === (state.total / 2)) {
-              // send them out, might also create groups again.
-              for (var key in state.group) {
-                var g = chi.group('xout', output);
-                output({
-                  out: state.group[key],
-                  by: $.create(JSON.parse(key)) // same as $.by
-                }, g.item());
-                g.done();
-              }
-            }
+            state.groupBy.setBy($.by)
           }.call(this);
           return {
             state: state,
@@ -161,5 +65,34 @@ module.exports = {
       }
     }
   },
-  state: {}
+  dependencies: {
+    npm: {
+      "chix-group": require('chix-group')
+    }
+  },
+  state: {},
+  on: {
+    start: function __ONSTART__(data, source, state, input, $, output, chix_group) {
+      var r = function() {
+        state.groupBy = chix_group.groupBy.create()
+        state.groupBy.on('group', function(group, by) {
+          var g = chix_group.send.create()
+          output({
+            xout: g.open()
+          })
+          output({
+            out: g.write($.create(group)),
+            by: $.create(by)
+          })
+          output({
+            xout: g.close()
+          })
+        })
+      }.call(this);
+      return {
+        state: state,
+        return: r
+      };
+    }
+  }
 }
--- nodes/groupBy/node.js
+++ nodes/groupBy/node.js
@@ -1,124 +1,22 @@
-state = {
-  group: {},
-  total: 0
-};
+on.start = function () {
+  state.groupBy = chix_group.groupBy.create()
+  state.groupBy.on('group', function (group, by) {
+    var g = chix_group.send.create()
+    output({xout: g.open()})
+    output({out: g.write($.create(group)), by: $.create(by)})
+    output({xout: g.close()})
+  })
+}
 
-// If this even works, state is not cleared
-// properly or at lease during send out
-// the current groups can be removed.
 
-// really needs a main also.
-// main is called always then.
-// and check the existance of those keys
-// first just make the synchronous one
 on.input.in = function () {
-  // x contains our keys
-  if (!x.hasOwnProperty(state.gid)) {
-    console.log(x)
-    throw Error('groupBy:in State gid not initialized yet', state.gid)
-  }
-  var id = x[state.gid];
-  if (!state[id]) {
-    state[id] = {};
-  }
+  state.groupBy.add($.in)
+}
 
-  state[id].in = $.get('in');
-
-  if (state[id].by) {
-    // we have a match.
-    if (!state.group[state[id].by]) {
-      state.group[state[id].by] = [];
-    }
-    state.group[state[id].by].push(state[id].in);
-  }
-
-  state.total++;
-
-  if (state.complete && state.length === (state.total / 2)) {
-    // send them out, might also create groups again.
-    for (var key in state.group) {
-      var g = chi.group('xout', output);
-
-      // like to introduce the grouped output, output like the below is grouped
-      // it's a bit like an array port, only difference the output goes to normal ports.
-      // I would like to add the convention, that if you send output this way.
-      // You must declare a group inside the json
-      //
-      // out: "group": "result"
-      // by:  "group": "result"
-      //
-      // in a ui this will look like:
-      // [o o] o o o
-      //
-      // Only two states:
-      //
-      //  [• •] and [o o]
-      //
-      // Whereas the individual ports are not perse synchronous:
-      //
-      //  [o o] o • o
-      //
-      // Where the first is the grouped output, you are assured those ports
-      // are pairs of data comming out, belong to eachother.
-      output({
-        out:$.create(state.group[key]),
-        by: $.create(JSON.parse(key)) // same as $.by
-      }, g.item());
-      g.done();
-    }
-  }
-};
-
-// collect the keys.
 on.input.by = function () {
-  if (!x.hasOwnProperty(state.gid)) {
-    console.log(x)
-    throw Error('groupBy:by State gid not initialized yet', state.gid)
-  }
+  state.groupBy.setBy($.by)
+}
 
-  var id = x[state.gid];
-  if (!state[id]) state[id] = {};
-
-  state[id].by = JSON.stringify($.by); // keyify
-
-  if (state[id].in) {
-    // we have a match.
-    if (!state.group[state[id].by]) state.group[state[id].by] = [];
-    state.group[state[id].by].push(state[id].in);
-  }
-
-  state.total++;
-
-  if (state.complete && state.length === (state.total / 2)) {
-    // send them out, might also create groups again.
-    for (var key in state.group) {
-      var g = chi.group('xout', output);
-      output({
-        out: state.group[key],
-        by: $.create(JSON.parse(key)) // same as $.by
-      }, g.item());
-      g.done();
-    }
-  }
-
-};
-
-// xin now kinda acts like a main.
-// more clear would probably be group begin and end methods.
-// it also makes it required to have this connected.
 on.input.xin = function () {
-  // will always arrive first.
-  // probably this can all be done outside a component.
-  // the pair matching is something common.
-  if (!$.xin.complete) {
-    // register the groupId
-    // we rely on this being set first
-    state.gid = $.xin.gid;
-    state.complete = false;
-  } else {
-    // is finished
-    // send it out.
-    state.complete = true;
-    state.length = $.xin.items.length;
-  }
-};
+  state.groupBy.receive($.xin)
+}
